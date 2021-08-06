d58a89a2bea39423c036e0ad48996fe592cedecd
Arnaldo Stanzani <arnaldo.stanzani@gmail.com>
2021-08-06 12:12:21 -0300

    Initial setup

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Auth } from '@aws-amplify/auth';
+import { REACT_APP_USER_POOL_ID, REACT_APP_USER_POOL_CLIENT_ID } from '@env';
+
+import { Feed, SignIn } from './src/screens';
+
+Auth.configure({
+  userPoolId: REACT_APP_USER_POOL_ID,
+  userPoolWebClientId: REACT_APP_USER_POOL_CLIENT_ID,
+});
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Feed />
       <StatusBar style="auto" />
     </View>
   );
---
+++ types/env.d.ts
@@ -0,0 +1,5 @@
+declare module '@env' {
+  export const REACT_APP_USER_POOL_ID: string;
+  export const REACT_APP_USER_POOL_CLIENT_ID: string;
+  export const REACT_APP_API_ENDPOINT: string;
+}
