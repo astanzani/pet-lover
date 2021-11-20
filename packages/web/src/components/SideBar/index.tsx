4957e2b35e688d6e6eec51383c99c3bb60ecaed7
Arnaldo Stanzani <arnaldo.stanzani@gmail.com>
2021-11-20 09:55:43 -0300

    Initial suggested pets side bar

--- packages/web/src/components/SideBar/index.tsx
+++ packages/web/src/components/SideBar/index.tsx
@@ -1,5 +1,39 @@
 import React from 'react';
+import { Box } from '@mui/material';
+
+import { Pet, useGetSuggestedPetsQuery } from 'generated/graphql';
+import { PaginatedList } from 'components';
 
 export function SideBar() {
-  return <div>Sidebar</div>;
+  const { data, loading, error, fetchMore } = useGetSuggestedPetsQuery();
+
+  if (loading) {
+    return null;
+  }
+
+  if (error || !data) {
+    const e = error ?? new Error('failed to get suggested pets');
+    throw e;
+  }
+
+  const { suggestedPets } = data;
+
+  const loadMore = async () => {
+    if (suggestedPets.cursor) {
+      await fetchMore({ variables: { cursor: suggestedPets.cursor } });
+    }
+  };
+  const renderItem = (item: Pet) => <>{item.name}</>;
+  const keyExtractor = (item: Pet) => item.petId;
+
+  return (
+    <Box>
+      <PaginatedList
+        items={suggestedPets.items}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
+        loadMore={loadMore}
+      />
+    </Box>
+  );
 }
